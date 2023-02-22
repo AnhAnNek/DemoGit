c161f1cf1b5b637421794e655d3f6e44bd5c771e
AnhAnNek <info.vananne@gmail.com>
2023-02-22 11:08:45 +0700

    #1 Add button to show message

--- DemoGit/Form1.cs
+++ DemoGit/Form1.cs
@@ -6,5 +6,10 @@ namespace DemoGit
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("a");
+        }
     }
 }
