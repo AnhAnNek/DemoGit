d165228c4da6d01a0fe2894fad0ac1b66fcf9f90
AnhAnNek <info.vananne@gmail.com>
2023-02-22 11:22:42 +0700

    #3 Add Teacher Class

---
+++ DemoGit/Teacher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoGit
+{
+    public class Teacher
+    {
+
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Teacher(string ID, string Name, int Age)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.Age = Age;
+        }
+    }
+}
