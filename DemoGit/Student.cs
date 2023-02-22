7a9eeecfb6178eb0642ebd311a543b0fc2d8106e
AnhAnNe <94521126+AnhAnNek@users.noreply.github.com>
2023-02-22 11:29:02 +0700

    #2 Add Student class (#4)
    Co-authored-by: ZaoQuynh <96408507+ZaoQuynh@users.noreply.github.com>

---
+++ DemoGit/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoGit
+{
+    public class Student
+    {
+        private string id;
+        private string name;
+        private int age;
+        public string ID
+        { get { return id; } set { id = value; } }
+        public string Name
+        { get { return name; } set { name = value; } }
+        public int Age
+        { get { return age; } set { age = value; } }
+    }
+}
